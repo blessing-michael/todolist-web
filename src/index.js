1f15d771946d9ba24c838506ee4f6d53cf13f789
Blessing <blessingmike334@gmail.com>
2023-08-18 12:10:36 +0100

    add setup file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const tasks = [
+  { desc: 'I have to read a book', completed: false, index: 1 },
+  { desc: 'I will clean my room', completed: true, index: 2 },
+  { desc: 'I will cook lunch at 10', completed: false, index: 3 },
+];
