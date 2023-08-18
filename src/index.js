ceeefef817c498b9dd0a20b1140dd057870c8508
Blessing <blessingmike334@gmail.com>
2023-08-18 12:14:12 +0100

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ const tasks = [
   { desc: 'I will clean my room', completed: true, index: 2 },
   { desc: 'I will cook lunch at 10', completed: false, index: 3 },
 ];
+
+function renderTasks() {
+  const mytodoList = document.getElementById('todo-list');
+  mytodoList.innerHTML = '';
+
+  tasks
+    .sort((a, b) => a.index - b.index)
+    .forEach((task) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = task.desc;
+      listItem.classList.add(task.completed ? 'completed' : 'pending');
+      mytodoList.appendChild(listItem);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderTasks);
